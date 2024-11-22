afcce7d635184db98b22388a752579c5a584dd1a
Ramer Alegre Ramos <ramzxs@gmail.com>
2024-11-22 22:47:25 +0800

    Checkboxes

---
+++ formprocessing/registration/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        label {
+            width: 100px;
+        }
+    </style>
+</head>
+<body class="container">
+    <h1>REGISTRATION</h1>
+
+    <form action="processor.php" method="post">
+        <label for="fullname">Name:</label>
+        <input type="text" name="fullname" id="fullname" required> <br>
+
+        <label for="dob">Birthdate:</label>
+        <input type="date" name="dob" id="dob"> <br>
+
+        <label>Gender:</label>
+        <input type="radio" name="gender" id="genderM" value="M"> <label for="genderM">Male</label>
+        <input type="radio" name="gender" id="genderF" value="F"> <label for="genderF">Female</label>
+        <br>
+
+        <label>Hobbies</label>
+        <input type="checkbox" name="hobby[]" value="S" id="hobby1"> <label for="hobby1">Singing</label>
+        <input type="checkbox" name="hobby[]" value="D" id="hobby2"> <label for="hobby2">Dancing</label>
+        <input type="checkbox" name="hobby[]" value="R" id="hobby3"> <label for="hobby3">Reading</label>
+
+        <label>Terms</label>
+        <input type="radio" name="agreed" id="agreedY" value="Y"> <label for="agreedY">Yes</label>
+        <input type="radio" name="agreed" id="agreedN" value="N"> <label for="agreedN">No</label>
+        
+        <label></label>
+        <button>Register Now</button>
+    </form>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ formprocessing/registration/processor.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <style>
+        label {
+            width: 100px;
+        }
+    </style>
+</head>
+<body class="container">
+    <h1>REGISTRATION</h1>
+
+    <?php
+    if ( isset($_POST['fullname']) ) { ?>
+
+        <table border="1" cellspacing="0" cellpadding="10">
+            <tr>
+                <td>Full Name:</td>
+                <td><?= $_POST['fullname'] ?></td>
+            </tr>
+            <tr>
+                <td>DOB:</td>
+                <td><?= $_POST['dob'] ?></td>
+            </tr>
+            <tr>
+                <td>Age:</td>
+                <td><?php
+                // TODO:
+                ?></td>
+            </tr>
+            <tr>
+                <td>Gender:</td>
+                <td>
+                    <?= 
+                        isset($_REQUEST['gender'])
+                            ?  ($_REQUEST['gender'] == 'M') ? 'Male' : 'Female'
+                            : ''
+                    ?>
+                </td>
+            </tr>
+            <tr>
+                <td>Hobbies</td>
+                <td>
+                    <?php
+                    if (isset($_REQUEST['hobby'])) {
+                        // print_r($_REQUEST['hobby']);
+                        // echo '<br>';
+                        // foreach ($arrayName as $keyName => $valueName) {}
+                        foreach ($_REQUEST['hobby'] as $key => $val) {
+                            switch ($val) {
+                                case 'S': echo 'Singing<br>'; break;
+                                case 'D': echo 'Dancing<br>'; break;
+                                case 'R': echo 'Reading<br>'; break;
+                            }
+                        }
+                    } ?>
+                </td>
+            </tr>
+        </table>
+
+        <?php
+    } else { ?>
+
+        <a href="index.php" class="btn btn-warning">You have to fill out the registration form first.</a>
+        
+        <?php
+    } ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
