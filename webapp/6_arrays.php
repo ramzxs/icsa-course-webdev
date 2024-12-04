144a42e7eb45bc28e0cd9ae83834dc7c7868d230
Ramer Alegre Ramos <ramzxs@gmail.com>
2024-12-05 00:16:47 +0800

    Array Sort

--- webapp/6_arrays.php
+++ webapp/6_arrays.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Arrays</title>
 </head>
+
 <body>
     <h1>ARRAYS</h1>
 
-    <p>Data Structures and Algorithm</p>
+    <p>Data Structures and Algorithms</p>
 
     <?php
     $var1 = 1;
@@ -20,19 +22,19 @@
     $numbers = [1, 2, 33, 44];
     //                0  1   3  4   5      // Key
     $numbers = array(55, 6, 77, 8, 99);    // Value
-
+    
     // Count
     $N = count($numbers);
-    echo $N.'<br>';
+    echo $N . '<br>';
 
     // Array Item Access
     // Access Operator [index]
-    echo $numbers[0].'<br>';    // First Item
-    echo $numbers[$N-1].'<br>'; // Last Item
-
+    echo $numbers[0] . '<br>';    // First Item
+    echo $numbers[$N - 1] . '<br>'; // Last Item
+    
     // List all items
     for ($i = 0; $i < $N; $i++) {
-        echo $numbers[$i].' ';
+        echo $numbers[$i] . ' ';
     }
 
     echo '<hr>';
@@ -40,28 +42,61 @@
     // Syntax: foreach($array as [$key =>] $value) { body }
     foreach ($numbers as $key => $val) { // Key-Value Pair
         // echo ($key + 1).'. '.$numbers[$key].'<br>';
-        echo ($key + 1).'. '.$val.'<br>';
+        echo ($key + 1) . '. ' . $val . '<br>';
     }
 
 
     // CHALLENGE
-    $sales = [100, 150, 90, 175, 200, 215, 230, 245, 250];
+    $sales = [100, 88, 90, 175, 215, 230, 255, 250];
 
-    echo '<hr>';
+    $N = count($sales);
     $totalSales = 0;
-    for ($i = 0; $i < count($sales); $i++) {
+    $min = $sales[0];
+    $max = $sales[0];
+
+    for ($i = 0; $i < $N; $i++) {
         $totalSales += $sales[$i];
-    }
-    echo "Total Sales: <b>$totalSales</b><br>";
+
+        if ($sales[$i] < $min) {
+            $min = $val;
+        }
+
+        if ($sales[$i] > $max) {
+            $max = $val;
+        }
+    }    
+
+    echo "<hr>Total Sales: <b>$totalSales</b><br>";
+    echo "<hr>Average Sale: <b>" . ($totalSales / $N) . "</b><br>";
+    echo "<hr>Minimum Sale: <b>$min</b><br>";
+    echo "<hr>Maximum Sale: <b>$max</b><br>";
+
+    // PSEUDOCODE
+    // $min = 99999;  [100000, 100001, 100002]
+    // $min = firstItem = 100000
+    // loop
+    //     if item < min
+    //         min = item
+    // print min
+    
+    // foreach ($sales as $key => $val) { // index => item
+    //     if ($val < $min) {
+    //         $min = $val;
+    //     }
+    
+    //     if ($val > $max) {
+    //         $max = $val;
+    //     } 
+    // }
+
+    $arr = [5, 1, 2, 4, 3];
+    echo '<hr>';
+    print_r($arr);
 
     echo '<hr>';
-    $averageSale = 0;
-
-    echo '<hr>';
-    $min = 0;
-
-    echo '<hr>';
-    $max = 0;
+    sort($arr);
+    print_r($arr);
     ?>
 </body>
+
 </html>
