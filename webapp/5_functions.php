b4a0342248a211b471bfa1303a8261ae0ec8b3f1
Ramer Alegre Ramos <ramzxs@gmail.com>
2024-12-04 22:05:03 +0800

    Functions

---
+++ webapp/5_functions.php
@@ -0,0 +1,53 @@
+<?php
+// PHP
+
+// PROCEDURAL PROGRAMMING
+
+
+/*
+FUNCTION DECLARATION
+function funcName(parametersList) {
+    body
+    return statement;
+} */
+
+function printTop() {
+    ?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    
+    <?php
+}
+
+
+function printBottom() {
+    ?>
+        
+    </body>
+    </html>
+
+    <?php
+}
+
+
+// FUNCTION CALL
+printTop();
+
+
+
+printBottom();
+
+
+// OBJECTED-ORIENTED PROGRAMMING
+/*
+Object1.step1();
+Object1.step2();
+Object2.process1();
+Object2.process2(); */
